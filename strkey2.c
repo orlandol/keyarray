fe9c1399e1f039776dc1247bae4d9a5275104590
Orlando Llanes <orlandol@veribox.net>
2019-10-22 03:36:45 -0400

    Added placeholder Key Array Examples.

---
+++ strkey2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include "keyarray.h"
+
+/*
+ *  File: strkey2.c
+ *  Status: Planned
+ *
+ *  Complex String Key Array Example
+ *    by Orlando Llanes
+ *
+ *  https://github.com/orlandol/keyarray
+ */
+
+/*
+ *  Main program
+ */
+int main( int argc, char* argv[] ) {
+  return 0;
+}
---
+++ uintkey1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include "keyarray.h"
+
+/*
+ *  File: uintkey1.c
+ *  Status: Planned
+ *
+ *  Simple Uint Key Array Example
+ *    by Orlando Llanes
+ *
+ *  https://github.com/orlandol/keyarray
+ */
+
+/*
+ *  Main program
+ */
+int main( int argc, char* argv[] ) {
+  return 0;
+}
