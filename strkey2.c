ab7190b0563342e4ae6e4dd976c8d052b0633750
Orlando Llanes <orlandol@veribox.net>
2019-10-23 03:57:11 -0400

    Started Complex String Key Array example.

--- strkey2.c
+++ strkey2.c
@@ -4,13 +4,39 @@
 
 /*
  *  File: strkey2.c
- *  Status: Planned
+ *  Status: Started
  *
- *  Complex String Key Array Example
+ *  Complex String Key Array Example: Word statistics
  *    by Orlando Llanes
  *
  *  https://github.com/orlandol/keyarray
  */
+
+/*
+ *  Word statistics declarations
+ */
+
+  typedef struct WordStatistics {
+    char* wordBeforeFirst;
+    unsigned firstLine;
+    unsigned firstColumn;
+    unsigned repeatCount;
+  } WordStatistics;
+
+  DECLARE_STRING_KEYARRAY_TYPES( WordList, WordStatistics )
+
+  void ReleaseStatistics( WordStatistics* data ) {
+    if( data ) {
+      if( data->wordBeforeFirst ) {
+        free( data->wordBeforeFirst );
+        data->wordBeforeFirst = NULL;
+      }
+
+      data->firstLine = 0;
+      data->firstColumn = 0;
+      data->repeatCount = 0;
+    }
+  }
 
 /*
  *  Main program
